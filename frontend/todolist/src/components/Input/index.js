4cbac43f43e51a6bcc5c16c58d82d5e0bfbdd745
sylvio <sylvio.migliorucci@gmail.com>
2020-12-20 10:33:57 -0300

    Frontend init

---
+++ frontend/todolist/src/components/Input/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Input() {
+  return <div>
+    <input type='text'></input>
+  </div>;
+}
+
+export default Input;
---
+++ frontend/todolist/src/pages/auth.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Input from '../components/Input'
+import styles from '../styles/Home.module.css'
+
+export default function Auth() {
+  return (
+  <>
+    <h1>Hello </h1>
+    <Input></Input>
+  </>
+  )
+}
